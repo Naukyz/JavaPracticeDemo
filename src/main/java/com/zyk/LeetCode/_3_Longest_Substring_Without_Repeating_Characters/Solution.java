bc99f5e72fbd3ea6c552c6c3ada27bc0c721646f
Naukyz <zhao_yukuan@163.com>
2017-08-13 23:15:44 +0800

    Commit

---
+++ src/main/java/com/zyk/LeetCode/_3_Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -0,0 +1,109 @@
+package com.zyk.LeetCode._3_Longest_Substring_Without_Repeating_Characters;
+
+import java.util.*;
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ * <p>
+ * Examples:
+ * <p>
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * <p>
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * <p>
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> index = new HashMap<>();
+        int i = 0, ans = 0;
+        Character c;
+        Integer in;
+        for (int j = 0; j < s.length(); j++) {
+            c = s.charAt(j);
+            in = index.get(c);
+            if (in != null) {
+                i = Math.max(i, in);
+            }
+            ans = Math.max(ans, j - i + 1);
+            index.put(c, j + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
+    }
+}
+
+class Solution1 {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+}
+
+class Solution2 {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution3 {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
+
+class Solution4 {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+}
--- src/main/java/com/zyk/Test.java
+++
@@ -1,7 +0,0 @@
-package com.zyk;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("C");
-    }
-}
